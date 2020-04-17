2a9ede003303bb24b312403c84ee94ca0bf86a49
JordanRhysSmith <jordanrhysgtr@gmail.com>
2020-04-17 14:18:08 +0100

    update our menu items to navigate withRouter

--- src/components/Directory/Directory.component.js
+++ src/components/Directory/Directory.component.js
@@ -8,36 +8,41 @@ const Directory = () => {
         {
             title: 'hats',
             imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-            id: 1
+            id: 1,
+            linkUrl: 'hats'
         },
         {
             title: 'jackets',
             imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-            id: 2
+            id: 2,
+            linkUrl: 'jackets'
         },
         {
             title: 'shoes',
             imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-            id: 3
+            id: 3,
+            linkUrl: 'shoes'
         },
         {
             title: 'womens',
             imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
             size: 'large',
-            id: 4
+            id: 4,
+            linkUrl: 'womens'
         },
         {
             title: 'mens',
             imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
             size: 'large',
-            id: 5
+            id: 5,
+            linkUrl: 'mens'
         }
     ])
 
     return (
         <div className='directory-menu'>
-            {sections.map(({title, imageUrl, size, id}) => (
-                <MenuItem key={id} imageUrl={imageUrl} size={size} title={title}/>
+            {sections.map(({id, ...otherSectionProps}) => (
+                <MenuItem key={id} {...otherSectionProps}/>
             ))}
         </div>
     )
