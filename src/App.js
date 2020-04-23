f5ef932c1098c0a9ea476452e3432d838e40a541
JordanRhysSmith <jordanrhysgtr@gmail.com>
2020-04-23 08:24:25 +0100

    updating app to add redirect to home when signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 
@@ -43,14 +43,22 @@ const App = (props) => {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInPage}/>
+        <Route exact path='/signin' render={
+          () => props.currentUser
+          ? <Redirect to='/'/>
+          : <SignInPage/>}
+        />
       </Switch>
     </div>
   );
 };
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
