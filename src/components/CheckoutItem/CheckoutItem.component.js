f9fffffa950897bf7fe5a3491d2999f5cc33e75e
JordanRhysSmith <jordanrhysgtr@gmail.com>
2020-05-01 09:59:43 +0100

    Previously dded "£" before wrong element

--- src/components/CheckoutItem/CheckoutItem.component.js
+++ src/components/CheckoutItem/CheckoutItem.component.js
@@ -14,10 +14,10 @@ const CheckoutItem = ({ cartItem, addItem, reduceQuantity, removeItem }) => {
             <span className='name'>{name}</span>
             <span className='quantity'>
                 <div className='arrow' onClick={() => reduceQuantity(cartItem)}>&#10094;</div>
-                <span className='value'>£{quantity}</span>
+                <span className='value'>{quantity}</span>
                 <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
             </span>
-            <span className='price'>{price}</span>
+            <span className='price'>£{price}</span>
             <div className='remove-button' onClick={() => removeItem(cartItem)}>&#10005;</div>
         </div>
     )
